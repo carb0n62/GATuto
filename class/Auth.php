f67142398b0b4480268055e21d6482c060159459
Romain <romain.pluciennik@viacesi.fr>
2017-06-09 11:56:03 +0200

    Refactoring 3

--- class/Auth.php
+++ class/Auth.php
@@ -44,4 +44,52 @@ class Auth
         }
     }
 
+    public function user(){
+        if (!$this->session->read('auth')){
+            return false;
+        }
+        return $this->session->read('auth');
+    }
+
+    public function connect($user){
+        $this->session->write('auth', $user);
+    }
+
+    public function connectFromCookie($db){
+        if (isset($_COOKIE['remember']) && !$this->user()){
+            $remember_token = $_COOKIE['remember'];
+            $parts = explode('==', $remember_token);
+            $user_id = $parts[0];
+            $user = $db->query('SELECT * FROM users WHERE id = ?', [$user_id])->fetch();
+            if ($user){
+                $expected = $user_id . '==' . $user->remember_token . sha1($user_id . 'heisenberg');
+                if ($expected == $remember_token){
+                    $this->connect($user);
+                    setcookie('remember', $remember_token, time() + 60 * 60 * 24 *7);
+                }
+            } else {
+                setcookie('remember', NULL, -1);
+            }
+        }
+    }
+
+    public function login($db, $username, $password, $remember = false){
+        $user = $db->query('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL', ['username' => $username])->fetch();
+        if (password_verify($password, $user->password)){
+            $this->connect($user);
+            if ($remember){
+                $this->remember($db, $user->id);
+            }
+            return $user;
+        }else{
+            return false;
+        }
+    }
+
+    public function remember($db, $user_id){
+        $remember_token = Str::random(250);
+        $db->query('UPDATE users SET remember_token = ? WHERE id = ?', [$remember_token, $user_id]);
+        setcookie('remember', $user_id . '==' . $remember_token . sha1($user_id . 'heisenberg'), time() + 60 * 60 * 24 * 7);
+    }
+
 }
--- login.php
+++ login.php
@@ -1,28 +1,19 @@
 <?php
 require 'includes/bootstrap.php';
-require_once 'includes/functions.php';
-reconnect_from_cookie();
-if (isset($_SESSION['auth'])){
-    header('Location: account.php');
-    exit();
+$auth = App::getAuth();
+$db = App::getDatabase();
+$auth->connectFromCookie($db);
+if ($auth->user()){
+    App::redirect('account.php');
 }
 if (!empty($_POST) || !empty($_POST['username']) && !empty($_POST['password'])){
-    require_once 'includes/db.php';
-    require_once 'includes/functions.php';
-    $req = $pdo->prepare('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
-    $req->execute(['username' => $_POST['username']]);
-    if (($user = $req->fetch(PDO::FETCH_OBJ)) && password_verify($_POST['password'], $user->password)){
-        $_SESSION['auth'] = $user;
-        $_SESSION['flash']['success'] = 'Vous êtes maintenant connecté.';
-        if ($_POST['remember']){
-            $remember_token = str_random(250);
-            $pdo->prepare('UPDATE users SET remember_token = ? WHERE id = ?')->execute([$remember_token, $user->id]);
-            setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id . 'heisenberg'), time() + 60 * 60 * 24 * 7);
-        }
-        header('Location: account.php');
-        exit();
-    } else {
-        $_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrect';
+    $user = $auth->login($db, $_POST['username'], $_POST['password'], isset($_POST['remember']));
+    $session = Session::getInstance();
+    if ($user){
+        $session->setFlash('success', 'Vous êtes maintenant connecté.');
+        App::redirect('account.php');
+    }else{
+        $session->setFlash('danger', 'Identifiant ou mot de passe incorrect');
     }
 }
 ?>
